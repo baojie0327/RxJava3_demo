64f188f63ff01a0caba3e59a6435a65c182260ed
Jackson <bj0327_work@163.com>
2020-11-16 16:57:30 +0800

    create

--- app/src/main/java/com/frank/rxjava3_demo/MainActivity.java
+++ app/src/main/java/com/frank/rxjava3_demo/MainActivity.java
@@ -1,13 +1,87 @@
 package com.frank.rxjava3_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+
+
+    private void initView() {
+        TextView tvCreate = findViewById(R.id.tv_create);
+        tvCreate.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_create:
+                createStart();
+                break;
+        }
+    }
+
+
+    private void createStart() {
+        Observable.create(new ObservableOnSubscribe<String>() {
+
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> emitter) throws Throwable {
+                emitter.onNext("0");
+                emitter.onNext("1");
+                emitter.onNext("2");
+                emitter.onNext("3");
+                emitter.onNext("4");
+                emitter.onNext("5");
+                emitter.onComplete();
+
+            }
+        }).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "Disposable="+d);
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.d(TAG, "onNext="+s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "complete");
+            }
+        });
+    }
+
+
 }
+
+
